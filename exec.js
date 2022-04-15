0ea1b013206482afcf78eda5ed474ffb32a1b146
Nicholas Yuan <NicholasNYuan@gmail.com>
2022-04-14 22:17:12 -0500

    add multiple value handling, improve stack behavior

--- exec.js
+++ exec.js
@@ -1,11 +1,11 @@
 const quicksort = require('./quicksort');
 const shuffle = require('./shuffle');
 
-const ARRAY_LENGTH = 20;
+const ARRAY_LENGTH = 11;
 
 const isSorted = array => {
   for( let i = 0; i < ARRAY_LENGTH - 1; i++ ){
-    if( array[i] + 1 !== array[i + 1] ) return false;
+    if( array[i] + 1 !== array[i + 1] && array[i] !== arr[i+1] ) return false;
   }
   return true;
 }
@@ -19,14 +19,30 @@ for( let i = 0; i < arr.length; i++ ){
 // a better shuffle
 shuffle(arr);
 
+arr.push(3, 3);
 
+
+console.log(arr)
 const str = `SORTED ${ARRAY_LENGTH} values:`;
-
 console.time(str);
 quicksort(arr, true); // second argument is verbose
 console.timeEnd(str);
-
 console.log(`isSorted: ${isSorted(arr)}`);
+console.log(arr)
+
+
+const arr2 = new Array(ARRAY_LENGTH);
+for( let i = arr2.length - 1; i >= 0; i-- ){
+  arr2[arr2.length - 1 - i] = i
+}
+arr2.push(3, 3);
+
+console.log(arr2);
+const str2 = `SORTED ${ARRAY_LENGTH} reverse-ordered values:`;
+console.time(str2);
+quicksort(arr2, true); // second argument is verbose
+console.timeEnd(str2);
+console.log(`isSorted: ${isSorted(arr2)}`);
 
 /*
 const array2 = genUnsortedArray(ARRAY_LENGTH);
--- quicksort.js
+++ quicksort.js
@@ -9,8 +9,10 @@ const quicksort = (arr, verbose = false) => {
   let iterationEndIndex;   // no idea if it makes a difference, but why wouldn't it?
   let pivot;
   let pivotIndex;
+  let pivotOffset;
 
   while( stack.length > 1 ){  // if we only have 1 index left, we're done!
+    console.log(stack);
     iterationEndIndex = stack.pop();    // we'll push front-first, pop back-first
     iterationStartIndex = stack.pop();
     //console.log(stack.join(','), iterationStartIndex,iterationEndIndex); 
@@ -21,33 +23,43 @@ const quicksort = (arr, verbose = false) => {
       // in this case, our indices are adjacent and sorted, or the same
       // so we need to discard this EndIndex, and restack the start index.
       // eventually we will be out of indexes, and our sort will be complete
-      stack.push(iterationStartIndex);
+      //stack.push(iterationStartIndex);
     } else {
       // sort between indexes
+      pivotOffset = 0;
       pivot = arr[iterationStartIndex];
       pivotIndex = iterationStartIndex;
       if (verbose) printPivot(pivot, pivotIndex, iterationStartIndex, iterationEndIndex, arr);
-      for( let i = iterationStartIndex; i <= iterationEndIndex; i++ ){
+      for( let i = iterationStartIndex + 1; i <= iterationEndIndex; i++ ){
         value = arr[i];
         if( value < pivot ){
-          arr[i] = arr[pivotIndex + 1]; // I could save ops by not swapping the pivot
-          arr[pivotIndex + 1] = pivot;  // until the end of the sort
+          arr[i] = arr[pivotIndex + pivotOffset + 1]; // I could save ops by not swapping the pivot
+          arr[pivotIndex + pivotOffset + 1] = pivot;  // until the end of the sort
           arr[pivotIndex] = value;      // however, this will visualize better.
           pivotIndex++;                 // if you put the printpivot in this if statement.
-        }                               //try it out! you can watch it sort!
+        } else if( value === pivot ){
+          arr[i] = arr[pivotIndex + pivotOffset + 1];
+          arr[pivotIndex + pivotOffset + 1] = value;
+          pivotOffset++;
+          console.log(pivotOffset)
+        } 
       }
       if (verbose) printPivot(pivot, pivotIndex, iterationStartIndex, iterationEndIndex, arr);
       // time to re-stack! front-first.
       // we push up to five numbers. These represent the start and ends of the two unsorted halves
       // plus the pivotIndex between them, which is now in the correct spot.
-      stack.push( iterationStartIndex );
-      if( pivotIndex - 1 > iterationStartIndex ) stack.push( pivotIndex - 1 );
-      stack.push( pivotIndex );
-      if( pivotIndex + 1 < iterationEndIndex ) stack.push( pivotIndex + 1 );
-      stack.push( iterationEndIndex );     
- 
-    }
 
+      if( iterationStartIndex + 1 !== iterationEndIndex ){
+        if( pivotIndex - 1 > iterationStartIndex){
+          stack.push( iterationStartIndex );
+          stack.push( pivotIndex - 1 );
+        }
+        if( pivotIndex + pivotOffset + 1 < iterationEndIndex ){ 
+          stack.push( pivotIndex + pivotOffset + 1 );
+          stack.push( iterationEndIndex );
+        }
+      }
+    }
   }
 
   return;
@@ -58,7 +70,7 @@ const quicksort = (arr, verbose = false) => {
 const printPivot = (pivot, pivotIndex, iterationStartIndex, iterationEndIndex, arr) => {
   let value, string, base;
   console.log( 
-    `pivot: ${pivot}\t— ` +
+    `pivot: ${pivot} ${iterationStartIndex}-${iterationEndIndex}\t— ` +
     ( arr.slice(0,iterationStartIndex).length ?
       arr.slice(0,iterationStartIndex).map(n => { // this allows the values to line up.
         string = ' ';
